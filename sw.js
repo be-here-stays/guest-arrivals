2c7c11e9c9c2af4c2cdc2551e61cf48d9e8c55ba
BarryT2026 <barry@be-here.travel>
2026-04-24 09:33:21 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Caches app shell for fast loading and offline UI.
 // Monday.com API calls are always fetched from the network.
 
-const CACHE    = 'be-here-v32';
+const CACHE    = 'be-here-v35';
 const PRECACHE = [
   '/guest-arrivals/hub.html',
   '/guest-arrivals/index.html',
